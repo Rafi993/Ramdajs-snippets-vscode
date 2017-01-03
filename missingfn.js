c25fd8b5b8ca90cf1f0a9358d9f0fbc1552efeee
rafi993 <fair7993@gmail.com>
2017-01-03 10:18:15 +0530

    missing fn list creator added

---
+++ missingfn.js
@@ -0,0 +1,23 @@
+/**
+ *  This Script gives a list of all fn for snippet have not been added 
+ */
+
+'use strict';
+
+const fs = require('fs'),
+      R = require('ramda'),
+      stripJsonComments = require('strip-json-comments');
+
+const functionList = R.keys(R);
+const snippetfn = JSON.parse(stripJsonComments(fs.readFileSync('./javascript.json', 'utf8')));
+const presentfn = R.values(R.map((snippet)=>snippet.prefix.substring(2), snippetfn));
+const missingfn = R.difference(functionList,presentfn)
+console.log(missingfn)
+
+fs.writeFile("./missingfn.txt", missingfn, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("missingfn has been saved to text file");
+}); 
